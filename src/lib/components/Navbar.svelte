5b1e87587fb977b381399c0229ae59f31f4ac2b5
Yeray Alonso <yeray@steema.com>
2023-11-20 11:02:04 +0100

    Use explicit width and height on image elements

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -12,7 +12,7 @@
 <nav class="bg-white border-gray-200 dark:bg-gray-800">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="logo_som_energia.svg" class="h-8" alt="SomEnergia Logo" />
+      <img src="logo_som_energia.svg" class="h-8 w-[59]" alt="SomEnergia Logo" />
       <span class="self-center text-2xl font-semibold whitespace-nowrap">Open Data</span>
     </a>
     <button
